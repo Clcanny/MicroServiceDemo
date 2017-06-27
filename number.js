bfce2ea47477f2b43a3bdec54245412858a053c2
demons <837940593@qq.com>
2017-06-27 16:16:58 +0800

    nodejs can get number from spring-simple-client-service'

--- number.js
+++ number.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const fetch = require('isomorphic-fetch')
+const getNumber = () => fetch('http://115.159.199.121:4444/simple-client-service/number').then((res) => res.json())
 app.get('/health', (req, res) => {  
     res.json({
 	status: 'UP'
@@ -7,5 +9,10 @@ app.get('/health', (req, res) => {
 })
 app.get('/number', function(request, response) {
     response.send('{number: 1}')
+})
+app.get('/number2', function(request, response) {
+    getNumber().then((x) => {
+	response.send(x)
+  }) 
 })
 app.listen(8000)

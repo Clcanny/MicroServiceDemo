53acd49211c14391a68c7e1b1500b69eff98884c
demons <837940593@qq.com>
2017-06-27 19:52:59 +0800

    no need of run.sh; find another way to do the same thing.

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,1 +1,4 @@
-docker run -t -i -p 1111:1111 --name nameserver ms ./home/MicroServiceDemo/nameServer && spring-boot:run
+docker run -t -i -p 1111:1111 --name nameServer ms /bin/bash -c "cd /home/MicroServiceDemo/nameServer; mvn spring-boot:run"
+docker run -t -i -p 2222:2222 --name simpleClient ms /bin/bash -c "cd /home/MicroServiceDemo/simpleClient; mvn spring-boot:run"
+docker run -t -i -p 3333:3333 --name complicatedClient ms /bin/bash -c "cd /home/MicroServiceDemo/complicatedClient; mvn spring-boot:run"
+docker run -t -i -p 4444:4444 --name sidecar ms /bin/bash -c "cd /home/MicroServiceDemo/sidecar; mvn spring-boot:run"

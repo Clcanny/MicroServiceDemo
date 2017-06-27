80787972f22429df94d854f10d20514d273c91c8
demons <837940593@qq.com>
2017-06-27 20:18:51 +0800

    fix run.sh bug

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,4 +1,4 @@
-docker run -t -i -p 1111:1111 --name nameServer ms /bin/bash -c "cd /home/MicroServiceDemo/nameServer; mvn spring-boot:run"
-docker run -t -i -p 2222:2222 --name simpleClient ms /bin/bash -c "cd /home/MicroServiceDemo/simpleClient; mvn spring-boot:run"
-docker run -t -i -p 3333:3333 --name complicatedClient ms /bin/bash -c "cd /home/MicroServiceDemo/complicatedClient; mvn spring-boot:run"
-docker run -t -i -p 4444:4444 --name sidecar ms /bin/bash -c "cd /home/MicroServiceDemo/sidecar; mvn spring-boot:run"
+docker run -d -p 1111:1111 --name nameServer ms /bin/bash -c "cd /home/MicroServiceDemo/nameServer; mvn spring-boot:run"
+docker run -d -p 2222:2222 --name simpleClient ms /bin/bash -c "cd /home/MicroServiceDemo/communicationClass; mvn package; mvn install:install-file -Dfile=target/communicationClass-1.0-SNAPSHOT.jar -DpomFile=pom.xml; cd /home/MicroServiceDemo/simpleClient; mvn spring-boot:run"
+docker run -d -p 3333:3333 --name complicatedClient ms /bin/bash -c "cd /home/MicroServiceDemo/communicationClass; mvn package; mvn install:install-file -Dfile=target/communicationClass-1.0-SNAPSHOT.jar -DpomFile=pom.xml; cd /home/MicroServiceDemo/complicatedClient; mvn spring-boot:run"
+docker run -d -p 4444:4444 --name sidecar ms /bin/bash -c "cd /home/MicroServiceDemo/sidecar; mvn spring-boot:run"

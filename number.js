d8d9f9da09bd2c05121ab7aa5c91830e0ba0bbd0
demons <837940593@qq.com>
2017-06-27 10:58:41 +0800

    import sidebar

--- number.js
+++ number.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-app.get('/', function(request, response) {
+app.get('/health', (req, res) => {  
+    res.json({
+	status: 'UP'
+    })
+})
+app.get('/number', function(request, response) {
     response.send('{number: 1}')
 })
 app.listen(8000)

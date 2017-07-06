6f3b2afb819cc957f91d919c21e5f381bcf94d86
demons <837940593@qq.com>
2017-07-07 02:34:35 +0800

    traceCallback works greatly with complicated client

---
+++ nameServer/src/main/java/demons/nameServer/NewController.java
@@ -0,0 +1,18 @@
+//package demons.nameServer;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * Created by demons on 2017/7/6.
+// */
+//@RestController
+//public class NewController {
+//
+//    @GetMapping("/hello")
+//    public @ResponseBody
+//    String sayHello() {
+//        return "Hello!";
+//    }
+//}

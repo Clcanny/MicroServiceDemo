6f686ea41b42fb290fc14dd13dd3f1722b97549b
demons <837940593@qq.com>
2017-07-07 10:36:00 +0800

    add install.sh to communicationClass and spring-cloud-netflix-eureka-server to help user install package to local maven repository

---
+++ communicationClass/install.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+mvn package
+mvn install:install-file -Dfile=target/communicationClass-1.0-SNAPSHOT.jar -DpomFile=pom.xml
+mvn clean
--- spring-cloud-netflix-eureka-server/install.sh
+++ spring-cloud-netflix-eureka-server/install.sh
@@ -1,1 +1,4 @@
+#/bin/sh
+mvn package
 mvn install:install-file -Dfile=target/spring-cloud-netflix-eureka-server-1.4.0.BUILD-SNAPSHOT.jar -DpomFile=pom.xml
+mvn clean

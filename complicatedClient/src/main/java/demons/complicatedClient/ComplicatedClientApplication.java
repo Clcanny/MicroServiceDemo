baa20a8651c30f776c15d914cbe0b8041cad89a8
demons <837940593@qq.com>
2017-07-07 16:17:11 +0800

    add zipkin client

--- complicatedClient/src/main/java/demons/complicatedClient/ComplicatedClientApplication.java
+++ complicatedClient/src/main/java/demons/complicatedClient/ComplicatedClientApplication.java
@@ -6,11 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
 // ComplicatedClient会调用SimpleClient提供的服务
 public class ComplicatedClientApplication {
@@ -33,5 +36,10 @@ public class ComplicatedClientApplication {
     @Bean
     public SimpleClientService simpleClientService() {
         return new SimpleClientService(simple_client_service_url);
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler() {
+        return new AlwaysSampler();
     }
 }
--- complicatedClient/src/main/java/demons/complicatedClient/ComplicatedController.java
+++ complicatedClient/src/main/java/demons/complicatedClient/ComplicatedController.java
@@ -1,16 +1,28 @@
 package demons.complicatedClient;
 
 import demons.communicationClass.IntegerWrapper;
+import demons.communicationClass.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.GET;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by demons on 2017/6/26.
  */
 @RestController
 public class ComplicatedController {
+
+    private static final Logger LOG = Logger.getLogger(ComplicatedController.class.getName());
+
+    @Autowired
+    @LoadBalanced
+    protected RestTemplate restTemplate;
 
     @Autowired
     protected SimpleClientService simpleClientService;
@@ -28,5 +40,12 @@ public class ComplicatedController {
     @ResponseBody
     public IntegerWrapper getTwo() {
         return numberFeignClient.getTwo();
+    }
+
+    @GetMapping("/three")
+    public IntegerWrapper getThree() {
+        IntegerWrapper one = restTemplate.getForObject("http://complicated-client-service/one", IntegerWrapper.class);
+        IntegerWrapper three = new IntegerWrapper(one.getNumber() + 2);
+        return three;
     }
 }

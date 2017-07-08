96a62d73cd31ba4b6c73b1e9c1f2723e55294883
demons <837940593@qq.com>
2017-07-08 19:22:52 +0800

    .

--- Docker/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export HOST=$(curl --retry 5 --connect-timeout 3 -s 115.159.199.121/latest/meta-data/local-hostname)
-export LOCAL_IP=$(curl --retry 5 --connect-timeout 3 -s 115.159.199.121/latest/meta-data/local-ipv4)
-exec "$@"

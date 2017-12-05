6c237667f803dfad7256df36b1a64fdfd32be355
demons <837940593@qq.com>
2017-12-05 22:07:53 +0800

    .

--- Docker/build.sh
+++ Docker/build.sh
@@ -4,3 +4,4 @@ docker build -t micro-service-demo .
 #docker stack deploy -c docker-compose.yml microservicedemo
 #docker stack rm microservicedemo
 #有可能是Docker为容器指定的IP地址，在另一个容器中不可访问
+#docker exec -ti ff3c sh -c "ping 10.0.0.9 -c 4"

55012df8dfc9d3e0b6af1d356dbeb5da3fb30cda
demons <837940593@qq.com>
2017-12-06 00:06:10 +0800

    cancel IPInterceptorConfiguration in simpleclient

--- simpleClient/src/main/java/demons/simpleClient/IPInterceptorConfiguration.java
+++ simpleClient/src/main/java/demons/simpleClient/IPInterceptorConfiguration.java
@@ -1,29 +1,29 @@
-package demons.simpleClient;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-
-/**
- * Created by demons on 2017/6/28.
- */
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class IPInterceptorConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/number")
-                // .permitAll();
-                .hasIpAddress("localhost");
-    }
-}
+//package demons.simpleClient;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//
+///**
+// * Created by demons on 2017/6/28.
+// */
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class IPInterceptorConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and().authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/number")
+//                // .permitAll();
+//                .hasIpAddress("localhost");
+//    }
+//}

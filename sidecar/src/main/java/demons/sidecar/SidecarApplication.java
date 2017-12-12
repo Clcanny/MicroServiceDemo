9f9f67f48ec1fddd40861352c91b538dda24b044
demons <837940593@qq.com>
2017-12-12 16:53:53 +0800

    add @EnableDisvoveryClient to sidecar

--- sidecar/src/main/java/demons/sidecar/SidecarApplication.java
+++ sidecar/src/main/java/demons/sidecar/SidecarApplication.java
@@ -2,10 +2,12 @@ package demons.sidecar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
 @SpringBootApplication
 @EnableSidecar
+@EnableDiscoveryClient
 public class SidecarApplication {
 
     public static void main(String[] args) {
